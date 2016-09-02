c2e5773a957670a9c5cd7b83283cfbd8beec1d61
trentedu <trent@Trents-MacBook-Air.local>
2016-09-02 15:14:32 +1000

    About Page Added : Minor Commit

---
+++ pages/about.php
@@ -0,0 +1,14 @@
+<?php
+    $pagetitle = "Pinelands Music School";
+    include "../inc/connect.php";
+    include "../inc/header.php";
+    include "../inc/nav.php";
+?>
+    <div class="content">
+        <h1>Content goes here</h1>
+
+    </div>
+    <!--end content-->
+<?php
+    include "../inc/footer.php";
+?>
