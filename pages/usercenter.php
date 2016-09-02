78cd270ac95f61212a666ef93e886bd22fcdf52c
Sathya Amarsee <sathya.amarsee@connect.qut.edu.au>
2016-09-02 16:54:42 +1000

    fixed usercenter tags

--- pages/usercenter.php
+++ pages/usercenter.php
@@ -4,7 +4,7 @@ include "../inc/connect.php";
 include "../inc/header.php";
 include "../inc/nav.php";
 
-echo'<h1> user center </h1>
+echo'<h1> user center </h1>';
 
 include "../inc/footer.php";
 ?>
