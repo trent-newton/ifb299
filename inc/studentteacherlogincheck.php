6286aa9e5324ba663829f45a588f9b02eea6b054
trentedu <trent@Trents-MacBook-Air.local>
2016-09-05 13:55:34 +1000

    Add FAQ Template

--- inc/studentteacherlogincheck.php
+++ inc/studentteacherlogincheck.php
@@ -4,7 +4,7 @@ if(!isset($_SESSION['userID'])) //If a user is not logged in
 	$_SESSION['error'] = "Please login to access this page.";
 	header('location:../pages/login.php');
 	exit();
-} elseif(!($_SESSION['accountType'] == "Student") || !($_SESSION['accountType'] == "Teacher") || !($_SESSION['accountType'] == "StudentAndTeacher")) {
+} elseif(!($_SESSION['accountType'] == "Student") && !($_SESSION['accountType'] == "Teacher") && !($_SESSION['accountType'] == "StudentAndTeacher")) {
     $_SESSION['error'] = "Only Students or Teachers may access this page.";
 	header('location:../pages/index.php');
 	exit();
