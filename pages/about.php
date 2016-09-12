690ff93c7a3ed62a71abafd6c93ed5a70fedeecb
Trent Newton <trent@Trents-MacBook-Air.local>
2016-09-13 08:54:17 +1000

    Tidy up of About Page and Dedicated Teachers Page

--- pages/about.php
+++ pages/about.php
@@ -14,76 +14,8 @@ Nullam dolor turpis, facilisis eu mattis eget, fringilla sed tortor. Integer in 
 
 <h2>Meet our Teachers</h2>
 <?php
-  $sql = 'SELECT * FROM users WHERE accountType="Teacher" OR accountType="StudentAndTeacher" ';
-  $result = mysqli_query($con, $sql);
-  while ($row = mysqli_fetch_array($result)) {
-
-    echo "<h3>";
-    echo $row['firstName'];
-    echo " ";
-    echo $row['lastName'];
-    echo "</h3>";
-
-    $userID = $row["UserID"];
-
-    $sql4 = "SELECT * FROM teacherreviews WHERE teacherreviews.teacherID = '$userID'";
-    $result4 = mysqli_query($con, $sql4);
-    $totalRating = 0;
-    $count = 0;
-    while ($row4 = mysqli_fetch_array($result4)) {
-      $totalRating = $totalRating + $row4['reviewRating'];
-      $count = $count + 1;
-    }
-    $averagedRating = $totalRating / $count;
-
-    echo '<script type="text/javascript">
-    $(function(){
-       var ratings=[';
-    echo $averagedRating;
-    echo'];
-        $.each(ratings,function(){
-            $("#star_rating")
-                .removeAttr("id")
-                .width(70*(this/5))
-        });
-    });
-    </script>';
-
-    echo '<div id="rating">
-       <div id="star_rating" style="overflow:hidden;width:70px">
-           <img src="../images/about/five_stars.png" style="width:70px;height:auto"/> <!-- width:260px;height:49px -->
-       </div>
-   </div>';
-
-    $sql2 = "SELECT * FROM languages WHERE languages.userID = '$userID'";
-    $result2 = mysqli_query($con, $sql2);
-    echo "<strong>Languages I speak: </strong><br />";
-    while ($row2 = mysqli_fetch_array($result2)) {
-      echo $row2['language'];
-      echo "<br />";
-    }
-
-    echo "<br />";
-
-    $sql3 = "SELECT * FROM instruments WHERE instruments.userID = '$userID'";
-    $result3 = mysqli_query($con, $sql3);
-    echo "<strong>Instruments you can learn to play from me: </strong><br />";
-    while ($row3 = mysqli_fetch_array($result3)) {
-      echo $row3['instrument'];
-      echo "<br />";
-    }
-
-echo "<a href='https://www.facebook.com/";
-echo $row['facebookId'];
-echo "'><img class='about_icon' src='../images/socialMedia/facebook.png' alt='Facebook Link'></a>";
-echo "<br />";
-echo "<a href='mailto:";
-echo $row['email'];
-echo "'><img class='mail_icon' src='../images/socialMedia/mail.png' alt='e-Mail Link'></a>";
-
-}
+  include "../inc/ourTeachers.php";
 ?>
-
 
     </div>
     <!--end content-->
--- pages/index.php
+++ pages/index.php
@@ -6,16 +6,16 @@
 ?>
  <div class="adminCenter">
     <h1>Welcome To Our Offical Website</h1> <h3>INSERT SLOGAN HERE</h3>
-    
+
     <div class="temp">
         <div class="temp"><img class="AdminCenterImage" src="../images/home-page-images/acoustic-guitar.png">
            <br><h1>INSTRUMENT HIRE</h1>
         </div>
 
-        <div class="temp"><img class="AdminCenterImage" src="../images/home-page-images/icon.png">
+        <a href="qualifiedTeachers.php"><div class="temp"><img class="AdminCenterImage" src="../images/home-page-images/icon.png">
            <br><h1>QUALIFIED TEACHERS</h1>
-        </div>
-            
+        </div></a>
+
         <div class="temp"><img class="AdminCenterImage" src="../images/home-page-images/consulting-message.png">
            <br><h1>TESTIMONIALS</h1>
         </div>
