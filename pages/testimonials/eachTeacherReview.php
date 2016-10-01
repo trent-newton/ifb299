7a226456fe1c57a1d65c4de145e9958513248107
Trent Newton <trent@Trents-MacBook-Air.local>
2016-10-01 13:27:50 +1000

    Added Testimonial Pages

---
+++ pages/testimonials/eachTeacherReview.php
@@ -0,0 +1,71 @@
+<?php
+// Get selected reviewID
+$userID = $_GET['userID'];
+    $pagetitle = "Our Teachers Testimonials | Pinelands Music School";
+    include "../../inc/connect.php";
+    include "../../inc/header.php";
+    include "../../inc/nav.php";
+?>
+
+ <div class="content ">
+    <h1 class="centered">Testimonials About
+      <?php
+      $sql2 = "SELECT users.firstName, users.lastName FROM users WHERE users.userID = $userID";
+      $result2 = mysqli_query($con, $sql2);
+      while ($row = mysqli_fetch_array($result2)) {
+        echo $row["firstName"];
+        echo " ";
+        echo $row["lastName"];
+      }
+      ?>
+    </h1>
+
+<?php
+$sql = "SELECT teacherreviews.*,users.firstName, users.lastName FROM users INNER JOIN teacherreviews ON users.userID = teacherreviews.teacherID WHERE teacherreviews.teacherID = $userID";
+$result = mysqli_query($con, $sql);
+
+while ($row = mysqli_fetch_array($result)) {
+  if ($row["reviewStatus"] == 'Public') {
+    echo '<div class="faq_post">
+					<div class="faq_copy">
+						<div class="padding">
+						<h2 class="faq_title">';
+            echo $row["reviewComment"];
+            echo "<br>";
+            $ratingStars = $row["reviewRating"];
+            echo '<script type="text/javascript">
+            $(function(){
+               var ratings=[';
+            echo $ratingStars;
+            echo'];
+                $.each(ratings,function(){
+                    $("#star_rating")
+                        .removeAttr("id")
+                        .width(120*(this/5))
+                });
+            });
+            </script>';
+
+            echo '<div id="rating">
+               <div id="star_rating" style="overflow:hidden;width:120px">
+                   <img src="../../images/about/five_stars.png" style="width:120px;height:auto"/> <!-- width:260px;height:49px -->
+               </div>
+           </div>';
+            echo '</h2>
+						<p>Review of ';
+            echo $row["firstName"];
+            echo " ";
+            echo $row["lastName"];
+            echo '<br />
+						</p>
+						</div>
+					</div>
+				</div>';
+        }
+      }
+ ?>
+</div>
+    <!--end content-->
+<?php
+    include "../../inc/footer.php";
+?>
---
+++ pages/testimonials/testimonials.php
@@ -0,0 +1,59 @@
+<?php
+    $pagetitle = "Testimonials | Pinelands Music School";
+    include "../../inc/connect.php";
+    include "../../inc/header.php";
+    include "../../inc/nav.php";
+?>
+ <div class="content ">
+    <h1 class="centered">Testimonials</h1>
+
+<?php
+  $sql = "SELECT teacherreviews.*,users.firstName, users.lastName FROM users INNER JOIN teacherreviews ON users.userID = teacherreviews.teacherID";
+  $result = mysqli_query($con, $sql);
+
+while ($row = mysqli_fetch_array($result)) {
+  if ($row["reviewStatus"] == 'Public') {
+    echo '<div class="faq_post">
+					<div class="faq_copy">
+						<div class="padding">
+						<h2 class="faq_title">';
+            echo $row["reviewComment"];
+            echo "<br>";
+            $ratingStars = $row["reviewRating"];
+            echo '<script type="text/javascript">
+            $(function(){
+               var ratings=[';
+            echo $ratingStars;
+            echo'];
+                $.each(ratings,function(){
+                    $("#star_rating")
+                        .removeAttr("id")
+                        .width(120*(this/5))
+                });
+            });
+            </script>';
+
+            echo '<div id="rating">
+               <div id="star_rating" style="overflow:hidden;width:120px">
+                   <img src="../../images/about/five_stars.png" style="width:120px;height:auto"/> <!-- width:260px;height:49px -->
+               </div>
+           </div>';
+            echo '</h2>
+						<p>Review of ';
+            echo $row["firstName"];
+            echo " ";
+            echo $row["lastName"];
+            echo '<br />
+							<a class="faq_readmore" href="eachTeacherReview.php?userID='.$row['teacherID'].'">More reviews on this Teacher...</a>
+						</p>
+						</div>
+					</div>
+				</div>';
+        }
+      }
+ ?>
+</div>
+    <!--end content-->
+<?php
+    include "../../inc/footer.php";
+?>
