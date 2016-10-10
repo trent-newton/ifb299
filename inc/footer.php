6fcaec0a314927b9712ef1e78f663f9facf8ebfb
Trent Newton <trent@Trents-MacBook-Air.local>
2016-10-10 13:47:37 +1000

    Copyright in Footer

--- inc/footer.php
+++ inc/footer.php
@@ -4,19 +4,23 @@
                     <a href="https://www.facebook.com">
                     <img  src="../../images/socialMedia/facebook.png" alt="Facebook Link">
                     </a>
-                    
+
                     <a href="https://www.facebook.com">
                     <img  src="../../images/socialMedia/instagram.png" alt="instagram Link">
                     </a>
-                    
+
                     <a href="https://www.facebook.com">
                     <img  src="../../images/socialMedia/twitter.png" alt="twitter Link">
                     </a>
-                    
+
                     <a href="https://www.facebook.com">
                     <img  src="../../images/socialMedia/snapchat.png" alt="snapchat Link">
                     </a>
-                 </div>
+            </div>
+            <div class="copyright">
+              <span>@ Copyright 2016 Pineland Music School</span>
+            </div>
+
         </footer>
 
 <br><br><?php var_dump($_SESSION);?>
