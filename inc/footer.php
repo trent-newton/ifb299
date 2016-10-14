16f1643edd97dbcd276ca26630df64d478018b59
Trent Newton <trent@Trents-MacBook-Air.local>
2016-10-14 14:13:44 +1000

    Footer Social Link Update

--- inc/footer.php
+++ inc/footer.php
@@ -5,15 +5,15 @@
                     <img  src="../../images/socialMedia/facebook.png" alt="Facebook Link">
                     </a>
 
-                    <a href="https://www.facebook.com">
+                    <a href="https://www.instagram.com">
                     <img  src="../../images/socialMedia/instagram.png" alt="instagram Link">
                     </a>
 
-                    <a href="https://www.facebook.com">
+                    <a href="https://www.twitter.com">
                     <img  src="../../images/socialMedia/twitter.png" alt="twitter Link">
                     </a>
 
-                    <a href="https://www.facebook.com">
+                    <a href="https://www.snapchat.com">
                     <img  src="../../images/socialMedia/snapchat.png" alt="snapchat Link">
                     </a>
             </div>
