9344024119bdec8dc897580fd1fdbe4d4681b3b6
Sathya Amarsee <sathya.amarsee@connect.qut.edu.au>
2016-10-17 11:10:03 +1000

    mainly functionality for teacher application review complete. table view is complete

--- pages/admin/adminReviewApplications.php
+++ pages/admin/adminReviewApplications.php
@@ -1,4 +1,4 @@
-<?php
+  <?php
     $pagetitle = "User Management";
     include "../../inc/connect.php";
     include "../../inc/header.php";
@@ -21,19 +21,25 @@
     <br>
 
 
-
     <table class="table" id="myTable" class="centerTable">
-      <th>Name<th><th>Age<th><th>Instruments<th><th>Languages<th><th><th>
+      <tr><th>Name</th><th>Age</th><th>Instruments</th><th>Languages</th><th>Availability</th><th></th></tr>
 <?php
+        $sql = "SELECT applications.userID, users.DOB, applications.language, applications.availability, users.firstname, users.lastname, instrumentnames.instrumentname
+        FROM applications INNER JOIN users ON applications.userID = users.userID
+        INNER JOIN instrumentnames ON instrumentnames.instrumentTypeID = applications.instrument";
+        $result = mysqli_query($con, $sql);
+
         while($row = mysqli_fetch_array($result))
         {
+          echo '<tr>';
           $age = floor((time() - strtotime($row['DOB'])) / 31556926);
           echo '<td>'.$row['firstname'].' '.$row['lastname'].'</td>';
           echo '<td>'.$age.'</td>';
-          echo '<td>'.$row['instruments'].'</td>';
-          echo '<td>'.$row['languages'].'</td>';
-          echo '<td>'.$row['instruments'].'</td>';
+          echo '<td>'.$row['instrumentname'].'</td>';
+          echo '<td>'.$row['language'].'</td>';
+          echo '<td>'.$row['availability'].'</td>';
           echo '<td><a href="../admin/adminViewFullApplication.php?userID='.$row['userID'].'"><span class="changeAccess"> Read more </span></a></td>';
+          echo '</tr>';
         }
 
 ?>
